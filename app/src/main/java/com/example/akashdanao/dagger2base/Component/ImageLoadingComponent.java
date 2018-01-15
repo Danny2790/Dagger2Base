349d08349a84159b28f7754e987a1111ff2793fe
Akash Danao <akash.danao@gmail.com>
2018-01-15 15:01:15 +0530

    link modules and component access component to activity class is done

--- app/src/main/java/com/example/akashdanao/dagger2base/Component/ImageLoadingComponent.java
+++ app/src/main/java/com/example/akashdanao/dagger2base/Component/ImageLoadingComponent.java
@@ -1,8 +1,16 @@
 package com.example.akashdanao.dagger2base.Component;
 
+import com.example.akashdanao.dagger2base.Module.PicassoModule;
+import com.squareup.picasso.Picasso;
+
+import dagger.Component;
+
 /**
  * Created by akash.danao on 15/01/18.
  */
 
+@Component(modules = {PicassoModule.class})
 public interface ImageLoadingComponent {
+
+    Picasso getPicasso();
 }
--- app/src/main/java/com/example/akashdanao/dagger2base/MainActivity.java
+++ app/src/main/java/com/example/akashdanao/dagger2base/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.akashdanao.dagger2base;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.example.akashdanao.dagger2base.Component.DaggerImageLoadingComponent;
+import com.example.akashdanao.dagger2base.Component.ImageLoadingComponent;
+import com.example.akashdanao.dagger2base.Module.ContextModule;
 import com.jakewharton.picasso.OkHttp3Downloader;
 import com.squareup.picasso.OkHttpDownloader;
 import com.squareup.picasso.Picasso;
@@ -11,13 +15,23 @@ import okhttp3.OkHttpClient;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = MainActivity.class.getSimpleName();
     private Picasso picasso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initAfterDagger();
+    }
 
+
+    public void initAfterDagger(){
+
+        ImageLoadingComponent daggerImageLoadingComponent = DaggerImageLoadingComponent.builder().contextModule(new ContextModule(this)).build();
+        picasso = daggerImageLoadingComponent.getPicasso();
+
+        Log.d(TAG, "initAfterDagger: picasso " + picasso);
     }
 
     public void InitBeforeDagger(){
