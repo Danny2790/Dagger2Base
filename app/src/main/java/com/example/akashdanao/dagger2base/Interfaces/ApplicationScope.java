a4db530deb9e55caeeca35ff5bee6180163eb312
Akash Danao <akash.danao@gmail.com>
2018-01-15 15:52:59 +0530

    create a scope interface to make all objects singleton

---
+++ app/src/main/java/com/example/akashdanao/dagger2base/Interfaces/ApplicationScope.java
@@ -0,0 +1,15 @@
+package com.example.akashdanao.dagger2base.Interfaces;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+/**
+ * Created by akash.danao on 15/01/18.
+ */
+
+@Scope
+@Retention(RetentionPolicy.CLASS)
+public @interface ApplicationScope {
+}
